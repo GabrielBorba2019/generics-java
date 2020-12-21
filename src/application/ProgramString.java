1c3a9a57e58c072f81201ac1b6cc648f6901953a
GabrielBorba2019 <gabrielborba.pm@Gmail.com>
2020-12-21 12:01:33 -0300

    Generics <T>

---
+++ src/application/ProgramString.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.util.Scanner;
+
+import model.services.PrintService;
+
+public class ProgramString {
+public static void main (String[]args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		//<Tipo generics> da coleção
+		//Type safefy do generic garante que sera retornado um String
+		PrintService<String> ps = new PrintService<>();
+		
+		System.out.print("How many values? ");
+		int n = sc.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			String value = sc.next();
+			ps.addValue(value);
+		}
+		
+		ps.print();
+		System.out.println("First: " + ps.first());
+		
+		sc.close();
+	}
+		
+}
